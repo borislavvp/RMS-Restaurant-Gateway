14ef11116fb0edebabc705e05fe4efdd6878a32d
Borislav Pavlov <416657@student.fontys.nl>
2021-04-16 22:27:14 +0200

    initial

---
+++ Gateway/Startup.cs
@@ -0,0 +1,59 @@
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Ocelot.Middleware;
+
+namespace Gateway
+{
+    public class Startup
+    {
+        public IConfiguration Configuration { get; }
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddAccessTokenManagement();
+
+            JwtSecurityTokenHandler.DefaultInboundClaimTypeMap.Clear();
+
+            var AuthenticationScheme = Configuration.GetValue<string>("AuthenticationScheme");
+            var Authority = "GloboTicketGatewayAuthenticationScheme";
+            var Audience = "GloboTicketGatewayAuthenticationScheme";
+
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                   .AddJwtBearer(AuthenticationScheme, options =>
+                   {
+                       options.Authority = Authority;
+                       options.Audience = Audience;
+                   });
+
+            services.AddHttpClient();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseOcelot();
+        }
+    }
+}
