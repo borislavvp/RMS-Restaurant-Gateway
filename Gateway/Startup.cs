586f6ca41bc003e39f74fa4d8f918ab5e0661b41
Borislav Pavlov <416657@student.fontys.nl>
2021-04-17 13:12:03 +0200

    changed some project settings

--- Gateway/Startup.cs
+++ Gateway/Startup.cs
@@ -10,6 +10,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Ocelot.DependencyInjection;
 using Ocelot.Middleware;
 
 namespace Gateway
@@ -43,6 +44,8 @@ namespace Gateway
                    });
 
             services.AddHttpClient();
+
+            services.AddOcelot();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
